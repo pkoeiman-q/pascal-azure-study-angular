59cbd63fe8a19b5bcd77d014bad028b687fc120d
Pascal Koeiman <pkoeiman@quintor.nl>
2024-04-25 15:29:30 +0200

    Add font awesome support

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -3,16 +3,20 @@ import { PORTFOLIO } from '../../mock-data/mock-portfolios';
 import { Portfolio } from '../../interfaces/Portfolio';
 import { CommonModule } from '@angular/common';
 import { WorkExperienceComponent } from '../work-experience/work-experience.component';
+import { faSuitcase } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [CommonModule, WorkExperienceComponent],
+  imports: [CommonModule, WorkExperienceComponent, FontAwesomeModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
   portfolio: Portfolio = PORTFOLIO;
+  faSuitcase = faSuitcase;
+
   constructor() {}
   ngOnInit(): void {
   }
